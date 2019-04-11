4a697fe251dc2a7ccf3aca30aaefa2975453a1cf
Lawliet0717 <51524320@qq.com>
2019-04-11 19:39:16 +0800

    Default Changelist

--- src/main/java/com/neo/controller/UploadController.java
+++ src/main/java/com/neo/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.neo.controller;
 
 import com.neo.util.OSSClientUtil;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.apache.log4j.Logger;
 
 /**
  * @author RLY
@@ -20,7 +22,7 @@ import java.nio.file.Paths;
 @Controller
 public class UploadController {
 
-
+     private static Logger logger = Logger.getLogger(UploadController.class);
     /**
      * 保存到项目根目录下
      */
@@ -46,20 +48,31 @@ public class UploadController {
         }
 
         try {
-            String fileName= OSSClientUtil.createFileName(file.getOriginalFilename());
-            fileName=OSSClientUtil.uploadFile(file.getInputStream(),fileName);
+            //上传标签
+            String label = DigestUtils.sha1Hex(new String(file.getBytes()));
+            boolean found = OSSClientUtil.isFileExist(label);
+            if(found == false) {
+                OSSClientUtil.uploadLabel(label);
+                String fileName= OSSClientUtil.createFileName(file.getOriginalFilename());
+                //上传文件
+                fileName=OSSClientUtil.uploadFile(file.getInputStream(),fileName);
 
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
-            //如果没有files文件夹，则创建
-            if (!Files.isWritable(path)) {
-                Files.createDirectories(Paths.get(UPLOAD_FOLDER));
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+                //如果没有files文件夹，则创建
+                if (!Files.isWritable(path)) {
+                    Files.createDirectories(Paths.get(UPLOAD_FOLDER));
+                }
+                //文件写入指定路径
+                Files.write(path, bytes);
+
+                redirectAttributes.addFlashAttribute("message",
+                        "You successfully uploaded '" + file.getOriginalFilename() + "'");
+            } else {
+                logger.info("OSS is already existing this label！");
+                redirectAttributes.addFlashAttribute("message",
+                         file.getOriginalFilename() + " is already exist");
             }
-            //文件写入指定路径
-            Files.write(path, bytes);
-
-            redirectAttributes.addFlashAttribute("message",
-                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
 
         } catch (IOException e) {
             e.printStackTrace();
